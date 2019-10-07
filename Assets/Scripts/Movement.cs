04b0dc3e39a97e06ac52230f817c872176e43854
DESKTOP-2C7U08D\RobG88 <robgeorge88@gmail.com>
2019-10-07 16:28:17 -0400

    Added Cube & movement script

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    GameObject cube;
+    float speed = 5.0f;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+    }
+}
